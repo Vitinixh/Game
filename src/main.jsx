507304134d58ca0c4dc130c9295374e7b173fcec
Vitinixh <Vitinixh@gmail.com>
2026-06-27 02:53:54 -0300

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import StarGirlGame from './StarGirlGame'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <StarGirlGame />
+  </React.StrictMode>
+)
